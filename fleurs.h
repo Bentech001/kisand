e091d43fd060014064f48169584ee88e741806c2
Bentech001 <82641366+Bentech001@users.noreply.github.com>
2021-09-05 20:44:46 +0200

    Update fleurs.h

--- fleurs.h
+++ fleurs.h
@@ -5,14 +5,9 @@
 #include <stdlib.h>
 extern void creercompte();
 extern void login();
-extern void consultercompte(int id);
 extern void retourner();
 extern int main();
 extern void affichage_clients();
-extern void depot (int idd);
-extern void retrait (int idr);
-extern void virement (int iv);
-extern void balance(int ib);
 
 class Fleurs
 {
